4f9d8a0a535308c3bf31b93e00988189728fb841
Oskar Tajchert <OskarInzynierka@gmail.com>
2025-11-13 21:23:09 +0000

    Added description in sources.yml Created whole model stg_stare_zdarzenia.sql

---
+++ models/staging/stg_stare_zdarzenia.sql
@@ -0,0 +1,24 @@
+with
+source as (
+    select * from {{ source('inzynierka', 'stare_zdarzenia') }}
+),
+
+final as (
+    select
+        try_cast(ticket_number as bigint) as id_kuponu,
+        id_event as id_spotkania,
+        cast(replace(odds_value_bet, ',', '.') as numeric(10,2))as kurs,
+        opportunity as mecz,
+        market as rodzaj_zakladu,
+        sport_name as dyscyplina,
+        league_name as nazwa_rozgrywek,
+        typ as co_obstawiono,
+        cast(odds_is_winning as int) as czy_wygrane_zdarzenie
+    from
+        source
+    where try_cast(ticket_number as bigint) is not null
+    and odds_is_winning is not null
+    and opportunity is not null
+)
+
+select * from final

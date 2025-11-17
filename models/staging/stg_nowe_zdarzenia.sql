1eabbc206fe6c356365c44a67355f8cdb7eb3c92
Oskar Tajchert <OskarInzynierka@gmail.com>
2025-11-17 17:09:19 +0000

    Changed column czy_wygrane_zdarzenie in stg_nowe_zdarzenia and stg_stare_zdarzenia Deleted columns gospodarz and gosc

--- models/staging/stg_nowe_zdarzenia.sql
+++ models/staging/stg_nowe_zdarzenia.sql
@@ -8,22 +8,21 @@ final as (
         odds_number as id_zdarzenia,
         number as id_kuponu,
         match_name as mecz,
-        trim(split(match_name, '-')[0]) as gospodarz,
-        trim(split(match_name, '-')[1]) as gosc,
         tournament_name as nazwa_rozgrywek,
         sport_name as dyscyplina,
         line_name as rodzaj_zakladu,
         cast(replace(odds, ',', '.') as numeric(10,2)) as kurs,
         zagrany_typ as co_obstawiono,
-        cast(case status_twojego_zdarzenia
-                when 'zwycięski' then 1
-                when 'przegrany' then 0
-            end
-        as int) as czy_wygrane_zdarzenie
+        case status_twojego_zdarzenia
+            when 'zwycięski' then 'tak'
+            when 'przegrany' then 'nie'
+            else 'anulowane'
+        end as czy_wygrane_zdarzenie
     from
         source
-    where 
-        status_twojego_zdarzenia in ('zwycięski', 'przegrany')
+    where 1=1
+    and try_cast(number as bigint) is not null
+    and match_name is not null
 )
 
 select * from final
--- models/staging/stg_stare_zdarzenia.sql
+++ models/staging/stg_stare_zdarzenia.sql
@@ -5,18 +5,23 @@ source as (
 
 final as (
     select
+        concat(ticket_number, '_', row_number() over (partition by ticket_number order by ticket_number)) as id_zdarzenia,
         try_cast(ticket_number as bigint) as id_kuponu,
-        cast(replace(odds_value_bet, ',', '.') as numeric(10,2))as kurs,
         opportunity as mecz,
-        market as rodzaj_zakladu,
-        sport_name as dyscyplina,
         league_name as nazwa_rozgrywek,
+        sport_name as dyscyplina,
+        market as rodzaj_zakladu,
+        cast(replace(odds_value_bet, ',', '.') as numeric(10,2)) as kurs,
         typ as co_obstawiono,
-        cast(odds_is_winning as int) as czy_wygrane_zdarzenie
+        case odds_is_winning
+            when 1 then 'tak'
+            when 0 then 'nie'
+            else 'anulowane'
+        end as czy_wygrane_zdarzenie
     from
         source
-    where try_cast(ticket_number as bigint) is not null
-    and odds_is_winning is not null
+    where 1=1
+    and try_cast(ticket_number as bigint) is not null
     and opportunity is not null
 )
 

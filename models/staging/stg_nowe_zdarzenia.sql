62207da8b53efc5ddae6078c56d597f348039433
Oskar Tajchert <OskarInzynierka@gmail.com>
2025-11-17 13:39:52 +0000

    Added 2 columns gospodarz and gosc Standardization for column czy_wygrane_zdarzenie Filter for czy_wygrane_zdarzenie

--- models/staging/stg_nowe_zdarzenia.sql
+++ models/staging/stg_nowe_zdarzenia.sql
@@ -7,16 +7,23 @@ final as (
     select
         odds_number as id_zdarzenia,
         number as id_kuponu,
-        sport_name as dyscyplina,
-        category_name as kraj,
-        tournament_name as nazwa_rozgrywek,
         match_name as mecz,
+        trim(split(match_name, '-')[0]) as gospodarz,
+        trim(split(match_name, '-')[1]) as gosc,
+        tournament_name as nazwa_rozgrywek,
+        sport_name as dyscyplina,
         line_name as rodzaj_zakladu,
         cast(replace(odds, ',', '.') as numeric(10,2)) as kurs,
         zagrany_typ as co_obstawiono,
-        status_twojego_zdarzenia as czy_wygrane_zdarzenie --ujednolicenie
+        cast(case status_twojego_zdarzenia
+                when 'zwycięski' then 1
+                when 'przegrany' then 0
+            end
+        as int) as czy_wygrane_zdarzenie
     from
         source
+    where 
+        status_twojego_zdarzenia in ('zwycięski', 'przegrany')
 )
 
 select * from final
